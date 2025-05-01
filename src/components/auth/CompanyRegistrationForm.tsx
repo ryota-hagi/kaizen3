ab00e96bc157eb1d8bd3e80dd91c91219b0ddef6
萩良太 <hagiryouta@hagiryoutanoMacBook-Air.local>
2025-05-01 20:30:10 +0900

    NextAuth.js参照の削除: CompanyRegistrationFormからnext-auth/reactのインポートを削除

--- src/components/auth/CompanyRegistrationForm.tsx
+++ src/components/auth/CompanyRegistrationForm.tsx
@@ -2,7 +2,6 @@
 
 import React, { useState, useEffect } from 'react'
 import { useRouter } from 'next/navigation'
-import { signIn } from 'next-auth/react'
 import Link from 'next/link'
 import { useUser } from '@/contexts/UserContext/context'
 
